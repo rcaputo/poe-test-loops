1092e7311156f0ef2941c4d841f7c09989cd03c3
Rocco Caputo <rcaputo@cpan.org>
2011-02-02 20:11:23 -0500

    Bump for release.

--- lib/POE/Test/Loops.pm
+++ lib/POE/Test/Loops.pm
@@ -6,7 +6,7 @@ use strict;
 use vars qw($VERSION);
 
 use vars qw($VERSION);
-$VERSION = '1.036_314'; # NOTE - Should be #.### (three decimal places)
+$VERSION = '1.040'; # NOTE - Should be #.### (three decimal places)
 
 use File::Spec;
 use File::Path;
